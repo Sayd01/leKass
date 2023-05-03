6bca8f415be6f2e80a28cc81fa02b9305f5e2371
sayd ouedraogo <Sayd01>
2023-05-03 14:30:43 +0000

    init

---
+++ src/test/java/ci/saydos/lekass/LeKassApplicationTests.java
@@ -0,0 +1,13 @@
+package ci.saydos.lekass;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LeKassApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
